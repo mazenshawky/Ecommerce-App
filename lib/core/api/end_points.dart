7b07f10b82332da2c13f9ff34f7bbc936ff80be6
Mazen Shawky <mazenshawqy@gmail.com>
2023-01-07 17:59:10 +0200

    Add cart endpoint

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -2,4 +2,6 @@ class EndPoints {
   static const String baseUrl = "https://fakestoreapi.com";
 
   static const String allProductsPath = "$baseUrl/products";
+
+  static const String userCartPath = "$baseUrl/carts/user/2";
 }
