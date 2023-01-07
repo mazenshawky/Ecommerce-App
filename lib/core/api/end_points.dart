58574acbb4a7d99b53c295654c596b99561aede4
Mazen Shawky <mazenshawqy@gmail.com>
2023-01-07 22:33:45 +0200

    Add product details end point

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,4 +4,7 @@ class EndPoints {
   static const String allProductsPath = "$baseUrl/products";
 
   static String userCartPath(int userId) => "$baseUrl/carts/user/$userId";
+
+  static String productDetailsPath(int productId) =>
+      "$baseUrl/products/$productId";
 }
