70786caa3f049763964146fbef8a8cf8f6f1bf54
Mazen Shawky <mazenshawqy@gmail.com>
2023-01-07 02:13:40 +0200

    Configure bloc
    - Add flutter bloc
    - Add bloc observer

--- lib/app/injection_container.dart
+++ lib/app/injection_container.dart
@@ -3,8 +3,11 @@ import 'package:ecommerce_app/core/api/api_consumer.dart';
 import 'package:ecommerce_app/core/api/app_interceptors.dart';
 import 'package:ecommerce_app/core/api/dio_consumer.dart';
 import 'package:ecommerce_app/core/network/network_info.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'bloc_observer.dart';
 
 final sl = GetIt.instance;
 
@@ -26,4 +29,9 @@ Future<void> init() async {
       ));
   sl.registerLazySingleton(() => InternetConnectionChecker());
   sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => AppBlocObserver());
+}
+
+void initBlocObserver() {
+  Bloc.observer = sl<AppBlocObserver>();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'app/injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  di.initBlocObserver();
   runApp(const EcommerceApp());
 }
